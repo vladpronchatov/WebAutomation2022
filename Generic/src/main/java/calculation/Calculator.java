05a7a1c15f480c13d5a62f840ea1b87faeb8cebf
mrahman <rahmanww@gmail.com>
2022-01-09 20:39:06 -0500

    automation framework setup

---
+++ Generic/src/main/java/calculation/Calculator.java
@@ -0,0 +1,16 @@
+package calculation;
+
+public class Calculator {
+
+    public int addition(int a, int b){
+        int total = a + b;
+
+        return total;
+    }
+
+    public int subtraction(int a, int b){
+        int total = a - b;
+
+        return total;
+    }
+}
