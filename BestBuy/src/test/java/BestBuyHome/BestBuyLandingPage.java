1a26eb0a421eb0d7fdb15d811ab74d6d5bbdc5f7
Vlad <vp1192@gmail.com>
2022-01-28 15:27:40 -0500

    cigna

--- BestBuy/src/test/java/BestBuyHome/BestBuyLandingPage.java
+++ BestBuy/src/test/java/BestBuyHome/BestBuyLandingPage.java
@@ -1,6 +1,9 @@
 package BestBuyHome;
 
 import base.CommonAPI;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 import org.testng.annotations.Test;
 import javax.swing.*;
 
@@ -10,5 +13,30 @@ public class BestBuyLandingPage extends CommonAPI {
     public void landOnHomePage()throws InterruptedException{
         System.out.println(driver.getTitle());
         Thread.sleep(2000);
+    }
+
+    @FindBy(how = How.CSS, using = ".c-button-unstyled.hamburger-menu-button")
+    public static WebElement sectionNavBarWebElement;
+
+    @FindBy(how = How.CSS, using = "#gh-search-input")
+    public static WebElement searchWebElement;
+
+    @FindBy(how = How.CSS, using = ".c-button.c-button-secondary.c-button-sm.sign-in-btn")
+    public static WebElement logInWebElement;
+
+    @FindBy(how = How.CSS, using = ".cia-form__controls")
+    public static WebElement logInSubmitWebElement;
+
+    public void clickOnSectionMenu(){
+        sectionNavBarWebElement.click();
+    }
+    public void clickOnSearch(){
+        searchWebElement.click();
+    }
+    public void clickOnLogIn(){
+        logInWebElement.click();
+    }
+    public void clickOnLogInSubmitButton(){
+        logInSubmitWebElement.click();
     }
 }
--- Cigna/src/main/java/cignapages/CignaLandingPage.java
+++ Cigna/src/main/java/cignapages/CignaLandingPage.java
@@ -1,4 +1,33 @@
 package cignapages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
 public class CignaLandingPage {
+
+    @FindBy(how = How.CSS, using = ".navbar-toggler.navbar-toggler-right.btn.btn-sm.btn-flat-secondary")
+    public static WebElement sectionNavBarWebElement;
+
+    @FindBy(how = How.CSS, using = "./html/body/div[2]/div[1]/div/div[1]/div/div[2]/div/div/div/header/div[2]/nav[1]/div/ul/li[4]/form/div/div/span/button")
+    public static WebElement searchWebElement;
+
+    @FindBy(how = How.CSS, using = ".btn.btn-sm.btn-primary")
+    public static WebElement logInWebElement;
+
+    @FindBy(how = How.CSS, using = "#loginbutton")
+    public static WebElement logInSubmitWebElement;
+
+    public void clickOnSectionMenu(){
+        sectionNavBarWebElement.click();
+    }
+    public void clickOnSearch(){
+        searchWebElement.click();
+    }
+    public void clickOnLogIn(){
+        logInWebElement.click();
+    }
+    public void clickOnLogInSubmitButton(){
+        logInSubmitWebElement.click();
+    }
 }
